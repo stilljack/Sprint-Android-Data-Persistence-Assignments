157c65e3107f0c18bd8df0dd02d717ffc257687c
jack seymour <stilljack@gmail.com>
2019-09-19 13:36:31 -0700

    careful...careful...

--- M04/Assignment/app/src/main/java/com/saucefan/stuff/readinglist/room/BookDBDAO.kt
+++ M04/Assignment/app/src/main/java/com/saucefan/stuff/readinglist/room/BookDBDAO.kt
@@ -1,15 +1,20 @@
 package com.saucefan.stuff.readinglist.room
 
+import androidx.room.*
 import com.saucefan.stuff.readinglist.model.Book
 
 //looks like this will mostly be annotations... man if this ends up being this easy...
 //i'll just be so happy!
-
+@Dao
 interface BookDBDAO {
-
+    //
+    @Insert(onConflict =  OnConflictStrategy.ABORT)
     fun createEntry(book: Book)
+    @Query("SELECT * FROM Book")
     fun readAllEntries(): MutableList<Book>
+    @Update(onConflict = OnConflictStrategy.REPLACE)
     fun updateEntry(book: Book)
+    @Delete
     fun deleteEntry(book: Book)
 
     /*
--- M04/Assignment/app/src/main/java/com/saucefan/stuff/readinglist/room/BookDBRepo.kt
+++ M04/Assignment/app/src/main/java/com/saucefan/stuff/readinglist/room/BookDBRepo.kt
@@ -1,3 +1,36 @@
 package com.saucefan.stuff.readinglist.room
 
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.Room
+import com.saucefan.stuff.readinglist.model.Book
+import com.saucefan.stuff.readinglist.viewmodel.StorageInterface
+
 //placeholder file for later embiggening
+
+class BookDBRepo (val context: Context) : StorageInterface {
+    override fun createEntry(book: Book) {
+        database.BookDBDAO().createEntry(book)
+    }
+
+    override fun updateEntry(book: Book) {
+        database.BookDBDAO().createEntry(book)
+    }
+
+    override fun deleteEntry(book: Book) {
+        database.BookDBDAO().deleteEntry(book)
+    }
+
+
+    override fun readAllEntries(): MutableList<Book>{
+        return database.BookDBDAO().readAllEntries()
+    }
+
+    // TODO 15: Build the Room database
+    private val database/**/ by lazy {
+        Room.databaseBuilder(
+                context.applicationContext,
+                BookDatabase::class.java, "book_database"
+        ).fallbackToDestructiveMigration().build()
+    }
+}
